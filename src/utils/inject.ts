1295c52a748b129dbf2cf024a34c1f90be6a5562
张城 <zhangcheng@zhangchengdeMacBook-Pro.local>
2023-06-07 23:16:36 +0800

    完成V3获取cookie的改造

--- src/utils/inject.ts
+++ src/utils/inject.ts
@@ -3,20 +3,36 @@ import { setSessionStorage } from '@/hooks'
 import { REQUEST_ACTION, STORAGE_KEY } from './../constants/common'
 
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.getSelected(function(tab) {
-    chrome.cookies.getAll(
-      {
-        url: 'https://eos.douyin.com',
-      },
-      function(cookies) {
-        let cookieStr = ''
-        for (let i = 0; i < cookies.length; i++) {
-          cookieStr += `${cookies[i].name}=${cookies[i].value}; `
-        }
-        console.log(cookieStr)
-        setSessionStorage(STORAGE_KEY.cookie, cookieStr)
+  // V2的写法
+  // chrome.tabs.getSelected(function(tab) {
+  //   chrome.cookies.getAll(
+  //     {
+  //       url: 'https://eos.douyin.com',
+  //     },
+  //     function(cookies) {
+        // let cookieStr = ''
+        // for (let i = 0; i < cookies.length; i++) {
+        //   cookieStr += `${cookies[i].name}=${cookies[i].value}; `
+        // }
+        // console.log(cookieStr)
+        // setSessionStorage(STORAGE_KEY.cookie, cookieStr)
+  //     }
+  //   );
+  // })
+
+  // V3的写法
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let url = tabs[0].url
+    chrome.cookies.getAll({ 
+      url
+    }, function (cookies) {
+      let cookieStr = ''
+      for (let i = 0; i < cookies.length; i++) {
+        cookieStr += `${cookies[i].name}=${cookies[i].value}; `
       }
-    );
-  })
+      console.log(cookieStr)
+      setSessionStorage(STORAGE_KEY.cookie, cookieStr)
+    });
+  });
 })
 export {}
